3c271e6377dafdcfa8b032cbf78139101eae97d6
Piyush <pshtiwari0@gmail.com>
2025-04-02 23:27:03 +0530

    Added Auth expose header in response

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
   // CORS headers
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Expose-Headers", "Authorization");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
